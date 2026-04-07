0dbef4a7e43810ed11d8b93c31e392136356eeec
Sangam <sangam26688@gmail.com>
2026-04-07 07:11:53 +0530

    Fixed API URL and Backend settings

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ 1. Middleware Settings (Updated for Production)
 const allowedOrigins = [
+    "https://sangam-p.vercel.app", // 👈 Apna ASLI frontend URL yahan dalo"
     "http://localhost:5173", 
     "https://sangam-protfolio-frontend.vercel.app" // 👈 Apna ASLI frontend URL yahan dalo
 ];
